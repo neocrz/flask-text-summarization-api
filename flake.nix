ae45c0edea7c17c94428ca06189d3fe7a853ccc2
neocrz <neo59crz@gmail.com>
2025-04-10 10:20:53 -0300

    init

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A Nix-flake-based Python development environment";
+
+  inputs = {
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "pinned-nixpkgs";
+  };
+  
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          venvDir = ".venv";
+          packages = with pkgs; [ 
+            python311
+          ] ++
+            (with pkgs.python311Packages; [
+              flask
+              pip
+              playwright
+              venvShellHook
+          ]);
+          postVenvCreation = ''
+            unset SOURCE_DATE_EPOCH
+          '';
+        };
+      });
+    };
+}
